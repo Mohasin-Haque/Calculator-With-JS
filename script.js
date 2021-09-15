17779b0d786bb65b0061f16882c99a50849b712e
AJIT SHARMA <ajit08082000@gmail.com>
2021-09-15 16:48:30 +0530

    committed to style
    Added hover features  to buttons and set a fixed container as well as applied background color  to look  charming

--- script.js
+++ script.js
@@ -6,20 +6,21 @@ const mulBtn = document.querySelector("#mul-btn");
 const divBtn = document.querySelector("#div-btn");
 const outputDiv = document.querySelector("#output");
 
-function addInputs(){
-  outputDiv.innerHTML = Number(inputOne.value) + Number(inputTwo.value);
+function addInputs() {
+    outputDiv.innerHTML = Number(inputOne.value) + Number(inputTwo.value);
 }
 
-function subInputs(){
-  outputDiv.innerHTML = Number(inputOne.value) - Number(inputTwo.value);
+function subInputs() {
+    outputDiv.innerHTML = Number(inputOne.value) - Number(inputTwo.value);
 }
 
-function mulInputs(){
-  outputDiv.innerHTML = Number(inputOne.value) * Number(inputTwo.value);
+function mulInputs() {
+    outputDiv.innerHTML = Number(inputOne.value) * Number(inputTwo.value);
+
 }
 
-function divInputs(){
-  outputDiv.innerHTML = Number(inputOne.value) / Number(inputTwo.value);
+function divInputs() {
+    outputDiv.innerHTML = Number(inputOne.value) / Number(inputTwo.value);
 }
 
 addBtn.addEventListener("click", addInputs)
